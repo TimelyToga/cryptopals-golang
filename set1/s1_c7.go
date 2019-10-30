809ceec94644cb148e0af2176caf997afbd33f6f
TimelyToga <timothy.blumberg@hotmail.com>
2019-10-30 15:11:02 -0700

    Finished S1C7

--- set1/s1_c7.go
+++ set1/s1_c7.go
@@ -1,7 +1,24 @@
 package set1
 
+import "crypto/aes"
+
 // DecryptAES128InECB is convoluted but is essentially using AES-128 in
 // Electronic Cookbook (ECB) mode.
 func DecryptAES128InECB(cipherText []byte, key []byte) []byte {
-	return
+	cipher, _ := aes.NewCipher(key)
+
+	// bytes
+	blockSize := 16
+	numBlocks := len(cipherText) / blockSize
+
+	decryptedBlock := make([]byte, len(cipherText))
+	for block := 0; block < numBlocks; block++ {
+		blockStart := block * blockSize
+		blockEnd := (block + 1) * blockSize
+		cipher.Decrypt(
+			decryptedBlock[blockStart:blockEnd],
+			cipherText[blockStart:blockEnd],
+		)
+	}
+	return decryptedBlock
 }
